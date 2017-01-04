1541d074377f7032f879d439eaf1801917ec3d41
mengchuan_zhang <mengchuan_zhang@kingdee.com>
2017-01-04 19:02:42 +0800

    change

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
-    owner        : 'wuyongxiu',
-    repo         : 'wuyongxiu.github.io',
+    owner        : 'mac-zhang',
+    repo         : 'mac-zhang.github.io',
     duoshuo      : 'wuyongxiu',
     baiduAnaly   : '9c3bc559412385c286e7caa93bdbbc04',
     // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
