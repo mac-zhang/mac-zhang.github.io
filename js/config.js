7cce8dfa779a79e9f54139543f7fc32c2fc2e2b6
mengchuan_zhang <mengchuan_zhang@kingdee.com>
2017-01-04 20:11:07 +0800

    修改配置文件

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 var CONFIG = {
     owner        : 'mac-zhang',
     repo         : 'mac-zhang.github.io',
-    duoshuo      : 'wuyongxiu',
-    baiduAnaly   : '9c3bc559412385c286e7caa93bdbbc04',
+    duoshuo      : 'maczhang',
+    baiduAnaly   : '1ad712c4213c670f99204a31fdbcbe2a',
     // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
 };
 
